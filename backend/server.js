67b1935045ea39c53033a6da6574e391f53643e3
Yuvraj Singh <yuvrajsingh7355091011@gmail.com>
2026-03-20 10:07:18 +0530

    chnages on msgOTP verification on partner app

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: '*',
-    credentials: true
+    origin: '*'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
